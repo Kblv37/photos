97be1d22374b2a7116a524f53d0e688e920479b9
Johongir <162979240+Kblv37@users.noreply.github.com>
2025-07-28 16:08:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const photos = [
-  { url: 'SAM_3246.jpg', uploadTime: new Date('2025-04-14') }
+  { url: 'SAM_3246.jpg', uploadTime: new Date('2025-04-14') },
+  { url: 'SAM_3236.JPG', uploadTime: new Date('2024-04-27T08:17') }
 ];
 
 const gallery = document.getElementById('gallery');
